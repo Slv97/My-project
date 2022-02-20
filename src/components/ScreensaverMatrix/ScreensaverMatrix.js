fd3026048bd89604f152e1663e2b3efdd28ace78
Slv97 <Slv97@mail.ru>
2022-02-20 15:59:59 +0300

    ScreensaverMatrix has been stabilized

--- src/components/ScreensaverMatrix/ScreensaverMatrix.js
+++ src/components/ScreensaverMatrix/ScreensaverMatrix.js
@@ -36,28 +36,29 @@ const ScreensaverMatrix = () => {
 
     const random = (items) => items[Math.floor(Math.random() * items.length)];
 
-    const hex = "0123456789ABCDEF".split("");
+    // const hex = "0123456789ABCDEF好GHJKLMNPQR女STVWXYZ".split("");
+    const hex = "0123456789ABCDEFLN".split("");
 
     useEffect(() => {
         const t = setInterval(() => {
             ctx.fillStyle = "rgba(0,0,0,.05)";
             ctx.fillRect(0, 0, w, h);
             ctx.fillStyle = "#0f0";
+
+            // ctx.font = "small-caps 24px Arial";
+            ctx.font = "small-caps 20px Arial";
+            
             p.map((v, i) => {
-                ctx.fillText(random(hex), i * 10, v);
-                p[i] = v >= h || v > 50 + 10000 * Math.random() ? 0 : v + 10;
+                ctx.fillText(random(hex), i * 20, v);
+                p[i] = v >= h || v > 50 + 10000 * Math.random() ? 0 : v + 20;
             });
-        }, 1000 / 30);
+        }, 1000 / 15);
         return () => clearInterval(t);
-    }, [ctx.fillStyle]);
+    }, [ctx]);
 
     return (
         <div className={s.wrap}>
-            <canvas ref={canvas} width={style.x} height={style.y} />
-            <p className={s.test}>width is : {size.x}</p>
-            <p className={s.test}>height is : {size.y}</p>
-            <p className={s.test}>canvas width is : {style.x}</p>
-            <p className={s.test}>canvas height is : {style.y}</p>
+            <canvas ref={canvas} width={style.x} height={style.y}/>
         </div>
     );
 };
